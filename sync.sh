f7acb44b8b1d135b78768889c1e11f51a350c09a
Mike De Smet <mike.desmet@viaa.be>
2018-07-25 18:28:32 +0200

    update pythonmodules, don't sync gmb

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync --exclude="*.pyc" --exclude='*.log' --exclude='.git/' --exclude='__pycache__/' --exclude='*.psql' -avz . do-tst-mke-01.do.viaa.be:./scripts/nvdgo-namelinking "$@"
+rsync --exclude="pythonmodules/ner/gmb/" --exclude='.idea/' --exclude="*.pyc" --exclude='*.log' --exclude='.git/' --exclude='__pycache__/' --exclude='*.psql' -avz . do-tst-mke-01.do.viaa.be:./scripts/nvdgo-namelinking "$@"
