f15ea9c299a8d37ce4edc67c7904225ed336ffc8
Mike De Smet <mike.desmet@viaa.be>
2018-08-05 15:06:31 +0200

    better formatting of output, remove ORGANISATION

--- testner.py
+++ testner.py
@@ -18,10 +18,10 @@ parser.add_argument('--skip', action='append',
                     help='Skip certain taggers, currently available taggers: %s' % ', '.join(NERFactory.KNOWN_TAGGERS))
 parser.add_argument('amount', type=int, default=10, nargs='?',
                     help='Test the taggers with AMOUNT phrases, 0 for all (default 10)')
+parser.add_argument('--no-output', dest='no_output', default=False, action='store_true',
+                    help='Don\'t output results')
 parser.add_argument('--save-html', dest='save_html', action='store_true',
-                    help='Save the confusion matrix in html files')
-parser.add_argument('--output-md', dest='output_md', action='store_true',
-                    help='Output results in MarkDown format')
+                    help='Save the results to html files')
 parser.add_argument('--save-md', action='store_true',
                     help='Save results as MarkDown documents')
 parser.add_argument('--log-level', type=str.upper, default='INFO', dest='log_level',
@@ -40,20 +40,25 @@ tabulate = partial(tabber, tablefmt='pipe', floatfmt='.%df' % precision, numalig
 rounder = partial(round_func, digit=precision)
 
 
-def stringify_result(tagger, result, links=None):
-    output = "## %s\n\n" % tagger
+def h(text, level=1, tablefmt='pipe'):
+    return ('#' * level) + ' ' + text + "\n"
+
+
+def stringify_result(tagger, result, links=None, tablefmt='simple'):
+    output = h(tagger, 2, tablefmt)
     data = (
         ('Accuracy', 'Time', 'Total checked'),
-        ('%.2f%%' % result['accuracy'], '%dms' % result['time'], result['total_checked'])
+        ('%.2f%%' % result.accuracy, '%dms' % result.time, result.total_checked)
     )
-    output += tabulate(data, headers='firstrow')
-    output += "\n\n### Confusion Matrix\n\n"
-    output += confusion_matrix_to_text(result['confusion_matrix'], links=links)
+    output += tabulate(data, headers='firstrow', tablefmt=tablefmt)
+    output += "\n\n"
+    output += h('Confusion Matrix', 3, tablefmt)
+    output += confusion_matrix_to_text(result.confusion_matrix, links=links, tablefmt=tablefmt)
     output += "\n"
     return output
 
 
-def confusion_matrix_to_text(cm, links=None):
+def confusion_matrix_to_text(cm, links=None, tablefmt='simple'):
     classes = cm.classes
     table = cm.table
     class_stat = cm.class_stat
@@ -65,19 +70,21 @@ def confusion_matrix_to_text(cm, links=None):
     data = [['Actual']]
     for key in classes:
         row = [table[key][i] for i in classes]
-        div = sum(row)
-        if div == 0:
-            div = 1
-        row = list(map(lambda x: ('%.' + str(precision - 2) + 'f%%') % (x/div*100), row))
+        # div = sum(row)
+        # if div == 0:
+        #     div = 1
+        # row = list(map(lambda x: ('%.' + str(precision - 2) + 'f%%') % (x/div*100), row))
         row.insert(0, key)
         data.append(row)
-    result = tabulate(data, headers=headers)
+    result = tabulate(data, headers=headers, tablefmt=tablefmt)
+    result += "\n\n"
 
-    result += "\n\n### Overall Statistics " + "\n\n"
+    result += h('Overall Statistics', 3, tablefmt)
     data = [(k.replace('_', ' '), rounder(v)) for k, v in overall_stat.items()]
-    result += tabulate(data, headers=('Name', 'Value'))
+    result += tabulate(data, headers=('Name', 'Value'), tablefmt=tablefmt)
+    result += "\n\n"
 
-    result += "\n\n### Class Statistics\n\n"
+    result += h('Class Statistics', 3, tablefmt)
     data = [['Class'] + list(map(str, classes)) + ['Description']]
     class_stat_keys = sorted(class_stat.keys())
     classes.sort()
@@ -90,28 +97,24 @@ def confusion_matrix_to_text(cm, links=None):
             row.append(PARAMS_DESCRIPTION[key].capitalize())
         row.insert(0, key)
         data.append(row)
-    result += tabulate(data, headers='firstrow')
+    result += tabulate(data, headers='firstrow', tablefmt=tablefmt)
     return result
 
 
 fmt = 'pipe'
 taggers = [tagger for tagger in NERFactory.KNOWN_TAGGERS if tagger not in args.skip]
-t = tester.Tester(taggers)
 
-# show the namedtuples as dicts instead of arrays for better readability
-results = [f._asdict() for f in t.test(args.amount)]
+results = tester.Tester(taggers).test(args.amount)
 
-if args.output_md:
-    print("# Results\n\n")
+if not args.no_output:
+    print(h("Results", tablefmt='fancy_grid'))
 
 for i, result in enumerate(results):
-    if args.output_md or args.save_md:
-        if args.output_md:
-            print(stringify_result(taggers[i], result))
-            print("")
-        if args.save_md:
-            with open('%s.md' % taggers[i], 'w') as f:
-                f.write(stringify_result(taggers[i], result, links=True))
+    if not args.no_output:
+        print(stringify_result(taggers[i], result, tablefmt='fancy_grid'))
+    if args.save_md:
+        with open('%s.md' % taggers[i], 'w') as f:
+            f.write(stringify_result(taggers[i], result, links=True, tablefmt='pipe'))
     if args.save_html:
-        result['confusion_matrix'].save_html(taggers[i])
+        result.confusion_matrix.save_html(taggers[i])
 
