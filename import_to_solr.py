d958e6cff189d8358f10476aaf6971c9e463aed2
Mike De Smet <mike.desmet@viaa.be>
2018-10-01 16:14:17 +0200

    auto determine where to continue from based on solr docs count

--- import_to_solr.py
+++ import_to_solr.py
@@ -3,12 +3,14 @@
 from argparse import ArgumentParser
 from pywebserver.lib.solrimport import BulkImporter
 import logging
+from pysolr import Solr
+from pythonmodules.config import Config
 
 
 parser = ArgumentParser(description='Bulk import ocr texts to solr')
 parser.add_argument('--continue-from', help='Continue from row CONTINUE_FROM')
 parser.add_argument('--debug', action='store_true', help='Show debug info')
-parser.add_argument('--test', action='store_true', help='Run test')
+parser.add_argument('--continue', action='store_true', dest='continue_', help='Continue (based on count in solr)')
 args = parser.parse_args()
 
 log_level = logging.DEBUG if args.debug else logging.INFO
@@ -19,6 +21,10 @@ logger.setLevel(log_level)
 
 if args.continue_from:
     start = int(args.continue_from)
+elif args.continue_:
+    solr = Solr(Config(section='solr')['url'])
+    res = solr.search(q='*:*', rows=1)
+    start = res.hits
 else:
     start = 0
 
