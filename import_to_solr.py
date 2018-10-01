6c348d0ad4eb20fcc68491f84d76af90a40379e8
Mike De Smet <mike.desmet@viaa.be>
2018-10-01 15:31:14 +0200

    migrate solrimport to seperate class, create non bulk version

--- import_to_solr.py
+++ import_to_solr.py
@@ -1,38 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from pythonmodules.mediahaven import MediaHaven
-
-from pythonmodules.config import Config
-from tqdm import tqdm
 from argparse import ArgumentParser
-from pythonmodules.namenlijst import Conversions
-from pythonmodules.profiling import timeit
-from collections import deque
-from binascii import crc32
-from pysolr import Solr
-from pythonmodules.multithreading import *
-
+from pywebserver.lib.solrimport import BulkImporter
 import logging
-from functools import partial
 
 
-def hasher(b):
-    return crc32(b.encode('utf-8'))
-
-
-parser = ArgumentParser(description='Import ocr texts to solr')
+parser = ArgumentParser(description='Bulk import ocr texts to solr')
 parser.add_argument('--continue-from', help='Continue from row CONTINUE_FROM')
 parser.add_argument('--debug', action='store_true', help='Show debug info')
 parser.add_argument('--test', action='store_true', help='Run test')
 args = parser.parse_args()
 
 log_level = logging.DEBUG if args.debug else logging.INFO
-logging.basicConfig(level=log_level)
+logging.basicConfig(level=log_level, format='%(asctime)s ' + logging.BASIC_FORMAT)
 logger = logging.getLogger(__name__)
 logger.propagate = True
 logger.setLevel(log_level)
-
-config = Config()
 
 if args.continue_from:
     start = int(args.continue_from)
@@ -42,84 +25,6 @@ else:
 if start:
     logger.info('Continuing from %d', start)
 
-
-def onslow(ms, is_slow, txt, *args):
-    global progress
-    if is_slow:
-        # logger.warning('[SLOW %s:%dms]', txt, ms)
-        progress.set_description('[SLOW %s:%dms]' % (txt, ms))
-        progress.refresh()
-        logger.warning('Slow processing "%s" %dms' % (txt, ms))
-    else:
-        logger.info('Processed %s in %dms' % (txt, ms))
-    pass
-
-
-timeit = partial(timeit, min_time=1000, callback=onslow)
-
-progress = tqdm()
-
-
-class ImportWords:
-    def __init__(self, buffer_size=10):
-        self._buffer = deque([[], []])
-        self._buffer_size = buffer_size
-        self._solr = Solr(Config(section='solr')['url'])
-        self._mh = MediaHaven()
-
-    def check_progress_buffer(self, force=False):
-        if not force and len(self._buffer[0]) < self._buffer_size:
-            return
-        self._buffer.append([])
-        buf = self._buffer.popleft()
-        logger.debug("Writing %d to solr", len(buf))
-        self._solr.add(buf)
-
-    def add(self, item):
-        self._buffer[0].append(item)
-        self.check_progress_buffer()
-
-    @multithreadedmethod(8, pbar=progress, pre_start=True, pass_thread_id=False)
-    def process(self, item):
-        real_idx, item = item
-        if item is None:
-            raise Exception("args: %s" % str(real_idx))
-        # logger.debug('process %d ' % real_idx)
-        pid = item['externalId']
-        if not pid:
-            raise "No pid for item %s" % (item,)
-        language = ''
-        try:
-            language = item['mdProperties']['language'][0].lower()
-        except Exception as e:
-            logger.warning('no language found for %s', pid)
-            logger.exception(e)
-        alto = self._mh.get_alto(pid)
-        if not alto:
-            logger.warning("no alto for #%d pid '%s' " % (real_idx, pid))
-            text = ''
-        else:
-            text = Conversions.normalize(alto.text)
-        self.add(dict(id=pid, text=text, language=language))
-
-    def start(self, start_from=None):
-        if start_from is None:
-            start_from = 0
-
-        with timeit('MH Search'):
-            if args.test:
-                data = [{"test": True} for a in range(0, 10)]
-            else:
-                data = self._mh.search('+(workflow:GMS) +(archiveStatus:on_tape)', start)
-
-            total = len(data) - start_from
-
-        progress.total = total
-        self.process(enumerate(data, 1 + start_from))
-        self.check_progress_buffer(force=True)
-        progress.close()
-
-
-importer = ImportWords(50)
-importer.start()
+importer = BulkImporter(50)
+importer.start(start)
 
---
+++ pywebserver/lib/solrimport.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+from pythonmodules.mediahaven import MediaHaven
+
+from pythonmodules.config import Config
+from tqdm import tqdm
+from pythonmodules.namenlijst import Conversions
+from pythonmodules.profiling import timeit
+from collections import deque
+from pysolr import Solr
+from pythonmodules.multithreading import *
+
+import logging
+from functools import partial
+
+logger = logging.getLogger(__name__)
+config = Config()
+
+
+class Importer:
+    def __init__(self):
+        self._solr = Solr(Config(section='solr')['url'])
+        self._mh = MediaHaven(buffer_size=100)
+
+    def add(self, item):
+        self._solr.add([item])
+
+    def process(self, item):
+        if item is None:
+            raise Exception("Invalid item passed (None)")
+
+        if type(item) is not str:
+            pid = item['externalId']
+        else:
+            pid = item
+            item = self._mh.one(pid)
+
+        if not pid:
+            raise "No pid for item %s" % (item,)
+
+        language = ''
+        try:
+            language = item['mdProperties']['language'][0].lower()
+        except Exception as e:
+            logger.warning('no language found for %s', pid)
+            logger.exception(e)
+
+        alto = self._mh.get_alto(item)
+        if not alto:
+            logger.warning("no alto for pid '%s' " % (pid,))
+            text = ''
+        else:
+            text = Conversions.normalize(alto.text)
+        self.add(dict(id=pid, text=text, language=language))
+
+
+class BulkImporter(Importer):
+    def __init__(self, buffer_size=10):
+        super().__init__()
+        self._buffer = []
+        self._buffer_size = buffer_size
+        self.progress = None
+        self.timeit = partial(timeit, min_time=1000, callback=self.onslow)
+
+    def onslow(self, ms, is_slow, txt, *args):
+        if is_slow:
+            # logger.warning('[SLOW %s:%dms]', txt, ms)
+            self.progress.set_description('[SLOW %s:%dms]' % (txt, ms))
+            self.progress.refresh()
+            logger.warning('Slow processing "%s" %dms' % (txt, ms))
+        else:
+            logger.info('Processed %s in %dms' % (txt, ms))
+
+    def check_progress_buffer(self, force=False):
+        if not force and len(self._buffer) < self._buffer_size:
+            return
+        buf, self._buffer = self._buffer, []
+        logger.debug("Writing %d to solr", len(buf))
+        self._solr.add(buf)
+
+    def add(self, item):
+        self._buffer.append(item)
+        self.check_progress_buffer()
+
+    @multithreadedmethod(8, queue_buffer_size=500, pre_start=True, pass_thread_id=False)
+    def process(self, item, *args, **kwargs):
+        idx, new_item = item
+        super().process(new_item, *args, **kwargs)
+
+    def start(self, start_from=None):
+        if start_from is None:
+            start_from = 0
+
+        self.progress = tqdm(initial=start_from)
+        with self.timeit('MH Search'):
+            data = self._mh.search('+(workflow:GMS) +(archiveStatus:on_tape)', start_from)
+            total = len(data)
+
+        self.progress.total = total
+        self.process._multithread.pbar = self.progress
+        self.process(enumerate(data, 1 + start_from))
+        self.check_progress_buffer(force=True)
+        self.progress.close()
+
+
